2071640b7664498fd25dd5848ed8aefd413b1912
Yann-Donald <yanndonald237@gmail.com>
2024-10-27 06:13:31 +0100

    Ready for deployment

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bottom-0 text-zinc-400">
+      All rights reserved, 2024 gni-design
+    </footer>
+  );
+};
+
+export default Footer;
