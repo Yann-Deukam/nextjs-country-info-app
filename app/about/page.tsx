ce12d8216fa5e9c39b53dcdeeb8a1adf2fa82a99
Yann-Donald <yanndonald237@gmail.com>
2024-10-27 07:16:40 +0100

    added about section

---
+++ app/about/page.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+const ScrollingText = () => {
+  return (
+    <>
+      <h1 className="font-bold text-4xl text-center my-10 sticky">About</h1>
+      <div className="fade mx-24">
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+        <p className="text-lg leading-loose font-semibold text-center">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ut
+          nobis, suscipit porro iste iusto molestias, eos tempora eveniet
+          eligendi ea ipsam sint fuga harum in perferendis quia aspernatur.
+          Officia! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quos ut nobis, suscipit porro iste iusto molestias, eos tempora
+          eveniet eligendi ea ipsam sint fuga harum in perferendis quia
+          aspernatur. Officia! Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Quos ut nobis, suscipit porro iste iusto molestias,
+          eos tempora eveniet eligendi ea ipsam sint fuga harum in perferendis
+          quia aspernatur. Officia!
+        </p>
+      </div>
+    </>
+  );
+};
+export default ScrollingText;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,23 @@
+// Footer.tsx
+
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 text-zinc-400">
-      All rights reserved, 2024 gni-design
+    <footer className="bg-zinc-800 text-zinc-200 py-4 mt-10 bottom-0">
+      <div className="flex justify-center">
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Gni-Design. All Rights Reserved.
+        </p>
+      </div>
+      <div className="flex justify-center mt-2">
+        <a href="/privacy" className="text-zinc-400 hover:text-zinc-200 mx-2">
+          Privacy Policy
+        </a>
+        <a href="/terms" className="text-zinc-400 hover:text-zinc-200 mx-2">
+          Terms of Service
+        </a>
+      </div>
     </footer>
   );
 };
